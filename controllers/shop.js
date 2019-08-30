47c0cdac7344bf19d5f515cf2aea3686f4ee677e
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2019-08-30 09:05:38 +0100

    added controller for product details

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,3 +10,21 @@ exports.getProducts = async (req, res, next) => {
     next(err);
   }
 };
+
+exports.getProduct = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      const error = new Error('Could not find product');
+      error.statusCode = 404;
+      next(error);
+    }
+    res.status(200).json(product);
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
--- routes/shop.js
+++ routes/shop.js
@@ -3,5 +3,6 @@ const shopController = require('../controllers/shop');
 const router = express.Router();
 
 router.get('/products', shopController.getProducts);
+router.get('/products/:id', shopController.getProduct);
 
 module.exports = router;
