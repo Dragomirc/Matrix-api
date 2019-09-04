198d4125335ad6f7accf52686c4925c7244e25a5
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2019-09-04 09:42:02 +0100

    implemented the login controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,10 @@
 const bcrypt = require('bcryptjs');
 const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 exports.signup = async (req, res, next) => {
+  console.log(req.body);
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const error = new Error('Validation failed.');
@@ -13,6 +15,7 @@ exports.signup = async (req, res, next) => {
   }
 
   const { name, email, password } = req.body;
+
   try {
     const hash = await bcrypt.hash(password, 12);
     const user = new User({
@@ -24,6 +27,42 @@ exports.signup = async (req, res, next) => {
     res
       .status(201)
       .json({ message: 'User succesfully created', userId: user._id });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Vlaidation failed.');
+    error.statusCode = 422;
+    return next(error);
+  }
+
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      const error = new Error('The email or password is incorrect.');
+      error.statusCode = 401;
+      return next(error);
+    }
+    const doMatch = await bcrypt.compare(password, user.password);
+    if (!doMatch) {
+      const error = new Error("The password doesn't match out records");
+      error.statusCode = 401;
+      return next(error);
+    }
+    const token = await jwt.sign(
+      { email: user.email, userId: user._id },
+      process.env.SECRET,
+      { expiresIn: '1h' }
+    );
+    res.status(200).json({ token, userId: user._id });
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { body } = require('express-validator/check');
+const { body } = require('express-validator');
 const User = require('../models/user');
 const authController = require('../controllers/auth');
 const router = express.Router();
@@ -28,4 +28,5 @@ router.post(
   ],
   authController.signup
 );
+router.post('/login', authController.login);
 module.exports = router;
